50073d432800273cdb0cbd9050449daa9179df3f
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2024-09-01 00:00:57 -0300

    Removido alguns imports que estavam apenas para testes

--- erp/erp/src/main/java/gestaorp/com/br/erp/cargos/controller/CargosController.java
+++ erp/erp/src/main/java/gestaorp/com/br/erp/cargos/controller/CargosController.java
@@ -3,8 +3,6 @@ package gestaorp.com.br.erp.cargos.controller;
 import gestaorp.com.br.erp.cargos.entities.Cargos;
 import gestaorp.com.br.erp.cargos.exeptions.Responses;
 import gestaorp.com.br.erp.cargos.services.CargosService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,20 +10,17 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cargos/")
-@Tag(name = "Cargos", description = "Gerenciamento de cargos")
 public class CargosController {
 
     @Autowired
     private CargosService cargosService;
 
     @PostMapping("")
-    @Operation(summary = "Cadastrar Cargo", description = "Cadastra um novo cargo")
     public ResponseEntity<Responses> cadastrar(@RequestBody Cargos cargo) {
         return this.cargosService.cadastrar(cargo);
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Alterar Cargo", description = "Altera um cargo existente")
     public ResponseEntity<Responses> alterar(@RequestParam Long id, @RequestBody Cargos cargo) {
         return this.cargosService.alterar(id, cargo);
     }
